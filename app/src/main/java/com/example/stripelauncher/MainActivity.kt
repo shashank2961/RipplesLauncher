34050ea8610542463a2459e62cfcd0c30e52e83e
shashank2961 <shashank2961@gmail.com>
2026-05-24 23:54:10 -0400

    Testing applications ability to record logs through button action.

--- app/src/main/java/com/example/stripelauncher/MainActivity.kt
+++ app/src/main/java/com/example/stripelauncher/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.stripelauncher
 
 import android.os.Bundle
+import androidx.compose.material3.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,30 +14,55 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.stripelauncher.ui.theme.StripeLauncherTheme
 
+// MainActivity Class inherits ComponentActivity, ensuring the app launches
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // This is where your UI is rendered
         setContent {
             StripeLauncherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    // Column setup
+                    androidx.compose.foundation.layout.Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+                        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center
+                    ) {
+
+                        // Title
+                        Text(text = "StripeLauncher")
+
+                        // Button
+                        androidx.compose.material3.Button(
+                            onClick = {
+                                println("Logged: Launch Game is triggered.")
+                            }
+                        ) {
+                            // Button Label
+                            Text(text = "Launch RetroArch")
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
+// Text
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Initiate launch game",
         modifier = modifier
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
